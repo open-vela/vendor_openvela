3681e0bb4fff634ab056993f12de1e2457972fba
liyanqi5 <liyanqi5@xiaomi.com>
2025-08-08 20:05:04 +0800

    vendor update
    Signed-off-by: liyanqi5 <liyanqi5@xiaomi.com>

--- boards/common/src/qemu_initialize.c
+++ boards/common/src/qemu_initialize.c
@@ -85,7 +85,11 @@ void qemu_board_initialize(void)
 }
 #endif
 
+<<<<<<< HEAD
 #ifdef CONFIG_ARCH_CHIP_GOLDFISH_ARM64
+=======
+#ifdef CONFIG_ARCH_CHIP_GOLDFISH
+>>>>>>> ba71fd1 (First commit)
 void goldfish_board_initialize(void)
 {
 }
