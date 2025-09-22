5a8ba849793d7cee08dbeac8bc311d1b3e20f1d1
Shanmin Zhang <zhangshanmin@xiaomi.com>
2025-09-22 11:16:16 +0800

    goldfish: rename CONFIG_ARCH_CHIP_QEMU to CONFIG_ARCH_CHIP_QEMU_ARM64
    Signed-off-by: Shanmin Zhang <zhangshanmin@xiaomi.com>
    Signed-off-by: v-tangmeng <v-tangmeng@xiaomi.com>

--- boards/common/src/qemu_initialize.c
+++ boards/common/src/qemu_initialize.c
@@ -80,7 +80,7 @@
  *
  ****************************************************************************/
 
-#ifdef CONFIG_ARCH_CHIP_QEMU
+#ifdef CONFIG_ARCH_CHIP_QEMU_ARM64
 void qemu_board_initialize(void)
 {
 }
