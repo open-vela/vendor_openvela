8e4928fd8da836ca15f0d8c4988a91a362fdd2ae
anpeiyun <anpeiyun@xiaomi.com>
2025-11-28 15:00:06 +0800

    common/src: support Sil for Openvela
    Modify common/src and support multi-core Sil for Openvela
    
    Signed-off-by: anpeiyun <anpeiyun@xiaomi.com>

--- boards/common/src/board.h
+++ boards/common/src/board.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- * vendor/sim/boards/common/src/customized.c
+ * vendor/openvela/boards/common/src/board.h
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
